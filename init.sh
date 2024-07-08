b21a0876f9213fe232c60149ceb11c210948f902
InSuperposition <f.galloway@gmail.com>
2024-07-08 21:25:40 +0300

    update brew file

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 export HOMEBREW_BUNDLE_FILE_GLOBAL=$XDG_CONFIG_HOME/homebrew/Brewfile
 export HOMEBREW_BUNDLE_FILE=$XDG_CONFIG_HOME/homebrew/Brewfile
 
-alias brew-lock="brew bundle dump --force && brew bundle"
+alias brew-bundle-lock="brew bundle dump --force && brew bundle install --no-upgrade"
+alias brew-bundle-upgrade="brew bundle dump --force && brew bundle install"
 
 eval "$(/usr/local/bin/brew shellenv)"
